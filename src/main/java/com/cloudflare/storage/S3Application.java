cf053c9d6c302e0e6fe48615cf6c9c5dcdfb22ed
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-09-23 00:28:23 +0800

    chore: 重命名项目

--- src/main/java/com/cloudflare/storage/S3Application.java
+++ src/main/java/com/cloudflare/storage/S3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaPlaygroundApplication {
+public class S3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaPlaygroundApplication.class, args);
+		SpringApplication.run(S3Application.class, args);
 	}
 
 }
