55c0495e67f23eb8a0b4fd8a9af104dc7e3ce533
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-09-29 15:43:52 +0800

    optimize: 优化 NOT_FOUND 页面云端部署

---
+++ src/main/java/com/cloudflare/storage/constant/ErrorPageConstants.java
@@ -0,0 +1,5 @@
+package com.cloudflare.storage.constant;
+
+public final class ErrorPageConstants {
+    public static final String NOT_FOUND = "https://not-found.sanwenyukaochi.cloudns.org/";
+}
--- src/main/java/com/cloudflare/storage/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cloudflare/storage/handler/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.cloudflare.storage.handler;
 
 import cn.hutool.core.text.StrBuilder;
+import com.cloudflare.storage.constant.ErrorPageConstants;
 import com.cloudflare.storage.model.Result;
 import com.cloudflare.storage.model.ResultCode;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.io.IOException;
 
 @Slf4j
 @RestControllerAdvice
@@ -29,5 +34,10 @@ public class GlobalExceptionHandler {
         );
         log.error(strBuilder.toString());
         return Result.error(ResultCode.ERROR);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public void handleNotFound(HttpServletResponse response) throws IOException {
+        response.sendRedirect(ErrorPageConstants.NOT_FOUND);
     }
 }
