133551367f31918bdf2f0d43853550722731043e
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-09-29 18:48:33 +0800

    optimize: 更新 NOT_FOUND 页面

--- src/main/java/com/cloudflare/storage/constant/ErrorPageConstants.java
+++ src/main/java/com/cloudflare/storage/constant/ErrorPageConstants.java
@@ -1,5 +1,5 @@
 package com.cloudflare.storage.constant;
 
 public final class ErrorPageConstants {
-    public static final String NOT_FOUND = "https://not-found.sanwenyukaochi.cloudns.org/";
+    public static final String NOT_FOUND = "https://error.sanwenyukaochi.cloudns.org/template/404";
 }
